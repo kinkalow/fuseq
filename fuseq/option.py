364ae9aa1113a1045ca03a0e40c9478bb576b5d5
kinkalow <kinkalow90@gmail.com>
2021-05-20 18:25:08 +0900

    Add config function and file

--- fuseq/option.py
+++ fuseq/option.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 import os
 import re
 from fuseq import __version__
@@ -11,44 +12,81 @@ class Option:
         self._check()
         self._create()
 
+    def _config(self, parser):
+        cfg_file = 'fuseq.cfg'
+        cfg1 = f'os.path.abspath(".")/{cfg_file}'
+        cfg2 = os.environ["FUSEQ_CFG"] if 'FUSEQ_CFG' in os.environ else ''
+        cfg3 = os.path.expanduser(f'~/.config/{cfg_file}')
+
+        # Check config file
+        if os.path.exists(cfg1):
+            cfg_path = cfg1
+        elif os.path.exists(cfg2):
+            cfg_path = cfg2
+        elif os.path.exists(cfg3):
+            cfg_path = cfg3
+        else:
+            return
+
+        # Load config file
+        cfg = configparser.ConfigParser()
+        cfg.read(cfg_path)
+
+        # Check section
+        section = 'fuseq'
+        if section not in cfg:
+            return
+
+        # Update self.args with a default value
+        args_dic = vars(self.args)
+        for key in cfg[section].keys():
+            if key in args_dic:
+                # Priority is an argument value rather than a config value
+                if args_dic[key] != parser.get_default(key):
+                    continue
+                try:
+                    if type(args_dic[key]) == bool:
+                        value = cfg.getboolean(section, key)
+                    elif isinstance(args_dic[key], int):
+                        value = cfg.getint(section, key)
+                    elif isinstance(args_dic[key], str):
+                        value = cfg[section][key]
+                    else:
+                        print('[Error] at config function')
+                        exit(1)
+                    args_dic[key] = value
+                except Exception:
+                    print(f'[Error] Invalid value "{cfg[section][key]}" for "{key}" in config file')
+                    exit(1)
+            else:
+                print(f'[Error] Invalid key "{key}" in config file')
+                exit(1)
+
     def _parse(self):
         prog = 'fuseq'
-
-        #class DelWorkAct(argparse.Action):
-        #    def __call__(self, parser, namespace, values, option_string=None):
-        #        if self.dest == 'delete_work':
-        #            delete = True
-        #        elif self.dest == 'no_delete_work':
-        #            delete = False
-        #        if 'tmp_delete_work' not in namespace:
-        #            setattr(namespace, 'tmp_delete_work', delete)
-        #        else:
-        #            namespace.tmp_delete_work = delete
 
         parser = argparse.ArgumentParser(prog=prog)
         parser.add_argument('genomon_root_dir', metavar='genomon_root_directory', type=str, help='Input directory')
         parser.add_argument('fuseq_root_dir', metavar='fuseq_root_directory', type=str, help='Output directory')
         parser.add_argument('-b', '--blat-opt', default='', type=str, help='Blat options')
-        #parser.add_argument('-b', '--blat-opt', default='-minScore=20 -minMatch=1', type=str, help='Blat options')
         parser.add_argument('-d', '--star-dir', default='', type=str, help='Alternative star directory in genomon output')
         parser.add_argument('-l', '--lines', default='', type=str, help='Line number in a fusion gene detection file')
         parser.add_argument('-f', '--fusion-file', default='', type=str, help='Alternative merge_fusionfusion_filt.txt file in genomon output')
         parser.add_argument('-p', '--coll-procs', default=4, type=int, help='Number of parallel processes for collecting data for Blat input')
         parser.add_argument('-w', '--no-delete-work', default=False, action='store_true', help='Do not delete work directory')
-        #parser.add_argument('-w', '--delete-work', default=False, action='store_true', help='Delete work directory')
-        #parser.add_argument('-w', '--delete-work', default=False, nargs=0, action=DelWorkAct, help='Delete work directory on restart')
         parser.add_argument('-B', '--restart-blat', default=False, action='store_true', help='Restart from Blat')
         parser.add_argument('-F', '--restart-filter', default=False, action='store_true', help='Restart from Blat filter')
         parser.add_argument('--no-use-filt', default=False, action='store_true', help='Use merge_fusionfusion.txt instead of merge_fusionfusion_filt.txt')
-        #parser.add_argument('--no-delete-work', default=True, nargs=0, action=DelWorkAct, help='Do not delete work directory when not restarting')
         parser.add_argument('--readname', default='', type=str, help='Filtering with readname')
         parser.add_argument('--sequence', default='', type=str, help='Filtering with sequence')
-        parser.add_argument('--reference', default='/share/pub/genomon/.genomon_local/genomon_pipeline-2.6.3/database/GRCh37/GRCh37.fa', type=str, help='Reference path')
-        parser.add_argument('--end', default=1, type=int, help='Extend the end position of breakpoints at Blat filtering')
         parser.add_argument('--start', default=0, type=int, help='Extend the start position of breakpoints at Blat filtering')
+        parser.add_argument('--end', default=1, type=int, help='Extend the end position of breakpoints at Blat filtering')
+        parser.add_argument('--reference', default='/share/pub/genomon/.genomon_local/genomon_pipeline-2.6.3/database/GRCh37/GRCh37.fa', type=str, help='Reference path')
         parser.add_argument('--time', default=False, action='store_true', help='Display elapsed time')
         parser.add_argument('--version', action='version', version=f'{prog}: {__version__}')
         self.args = parser.parse_args()
+
+        self._config(parser)
 
     def _check(self):
         args = self.args
@@ -102,14 +140,6 @@ class Option:
         del args.readname
         del args.sequence
         del args.no_use_filt
-        #
-        # Change Arg
-        #if 'tmp_delete_work' in args:
-        #    delete_work = args.tmp_delete_work
-        #    del args.tmp_delete_work
-        #else:
-        #    delete_work = args.delete_work if args.is_restart else args.no_delete_work
-        #args.delete_work = delete_work
 
         # New Arg
         args.is_restart = True if args.restart_blat or args.restart_filter else False
